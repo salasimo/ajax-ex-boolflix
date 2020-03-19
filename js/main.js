c5cc60d158774ae8bbe0dae3b4d58a9337e22727
Simone Sala <simonesala@me.com>
2020-03-19 15:54:58 +0100

    Aggiustamenti vari
    Create alcune funzioni, inserita possibilità di cercare con tasto invio

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
-
-$(document).ready(function(){
+$(document).ready(function() {
 
     var source = $("#results-template").html();
     var templateResults = Handlebars.compile(source);
 
-    $(".search-button").click(function(){
+    $(".search-button").click(function() {
+        deleteResults();
+        ajaxCall();
+    });
+
+    $(".input-query-box").keypress(function(){
+        if ((event.key == "Enter") && ($(this).val().trim() != "")){
+            deleteResults();
+            ajaxCall();
+        }
+    });
+
+    function deleteResults(){
         $(".box-results").html("");
+
+    };
+
+    function ajaxCall(){
         var apiBaseUrl = "https://api.themoviedb.org/3/search/movie";
         var chiaveRicerca = $(".input-query-box").val();
         $.ajax({
@@ -16,7 +31,7 @@ $(document).ready(function(){
                 query: chiaveRicerca
             },
             method: "GET",
-            success: function(data){
+            success: function(data) {
                 var movies = data.results;
                 for (var i = 0; i < movies.length; i++) {
                     var movie = movies[i];
@@ -24,33 +39,31 @@ $(document).ready(function(){
                     var title = movie.title;
                     var originalTitle = movie.original_title;
                     var language = movie.original_language;
-                    if (movie.vote_average > 0){
+                    if (movie.vote_average > 0) {
                         var vote = Math.ceil(movie.vote_average / 2);
-                        switch (vote) {
-                            case 1:
-                                vote = '<i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>';
-                                break;
-                            case 2:
-                                vote = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>';
-                                break;
-                            case 3:
-                                vote = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>';
-                                break;
-                            case 4:
-                                vote = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i>';
-                                break;
-                            case 5:
-                                vote = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>';
-                                break;
-                            default:
-                                vote = "ND";
-                                break;
+                    };
 
-                        };
+                    switch (vote) {
+                        case 1:
+                            vote = '<i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>';
+                            break;
+                        case 2:
+                            vote = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>';
+                            break;
+                        case 3:
+                            vote = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>';
+                            break;
+                        case 4:
+                            vote = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i>';
+                            break;
+                        case 5:
+                            vote = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>';
+                            break;
+                        default:
+                            vote = "ND";
+                            break;
 
                     };
-                    
-
 
 
                     var datiFilm = {
@@ -66,46 +79,13 @@ $(document).ready(function(){
                     $(".hai-cercato").show();
                     $(".hai-cercato span").html(chiaveRicerca);
                     $(".input-query-box").val("");
-                    // $(".titolo span").html(datiFilm.titolo)
-                    // $(".titolo-originale span").html(datiFilm.titoloOriginale)
-                    // $(".lingua span").html(datiFilm.lingua)
-                    // $(".voto span").html(datiFilm.voto)
                 }
             },
-            error: function(err){
+            error: function(err) {
                 alert("Errore");
             }
         });
-    });
-
-    function addStars(vote){
-        switch (vote) {
-            case 1:
-                vote = '<i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>';
-                break;
-            case 2:
-                vote = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>';
-                break;
-            case 3:
-                vote = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>';
-                break;
-            case 4:
-                vote = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i>';
-                break;
-            case 5:
-                vote = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>';
-                break;
-            default:
-                vote = "ND";
-                break;
-
-        };
     };
-
-
-
-// Quello che accade è che il valore in stelle del primo elemento viene dato anche a tutti gli altri,
-// tranne l'ultimo che rimane vuoto (non analizzato!)
 
 
 
