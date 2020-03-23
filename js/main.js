c641b6bdf4b8c4bb3728373d29b859e1cc3f0d76
Simone Sala <simonesala@me.com>
2020-03-23 19:23:37 +0100

    Riscritto JS + grafica = Milestone 4
    JS riscritto completamente, grafica di base completata. Dettagli ancora da perfezionare

--- js/main.js
+++ js/main.js
@@ -5,246 +5,187 @@ $(document).ready(function() {
     var templateResults = Handlebars.compile(source);
     //===============================================
 
-    $(".search-button").click(function() {
-        if ($(".input-query-box").val().trim() != ""){ //Avvio programma con tasto "cerca"
-            deleteResults();
-            ajaxCallFilm();
-            ajaxCallSerie();
+    // === VARIABILI ===
+
+    var apiBaseUrl = "https://api.themoviedb.org/3/search/";
+    var apiKey = "29ad81bd33ebd003f5307b9c32a17b2b";
+
+    //==================
+
+    $("body").on("click", ".box-ricerca.active .search-button", function() { //Avvio programma con bottone "Cerca"
+        if ($("body .box-ricerca.active .input-query-box").val().trim() != ""){
+            clearResults();
+            ajaxCall("movie");
+            ajaxCall("tv");
         };
     });
 
-    $(".input-query-box").keypress(function(){ //Avvio programma con "Invio"
+    $("body").on("keypress", ".box-ricerca.active .input-query-box", function(event){ //Avvio programma con tasto "Invio"
         if ((event.key == "Enter") && ($(this).val().trim() != "")){
-            deleteResults();
-            ajaxCallFilm();
-            ajaxCallSerie()
+            clearResults();
+            ajaxCall("movie");
+            ajaxCall("tv");
         }
     });
 
-    // FUNZIONI ===================================
+    $(".container").on("mouseover", ".card", function(){
+        $(this).find(".card-details").show();
+    });
+    $(".container").on("mouseleave", ".card", function(){
+        $(this).find(".card-details").hide();
+    });
 
-    function deleteResults(){
+    // ===== FUNZIONE CHIAMATA AJAX =====
+
+    function ajaxCall(tipo){
+        var chiaveRicerca = $("body .box-ricerca.active .input-query-box").val();
+        $.ajax({
+            url: apiBaseUrl + tipo,
+            data: {
+                api_key: apiKey,
+                language: "it-IT",
+                query: chiaveRicerca
+            },
+            method: "GET",
+            success: function(data) {
+                var risultati = data.results;
+                for (var i = 0; i < risultati.length; i++) {
+                    var elemento = risultati[i];
+
+                    var id = elemento.id;
+                    if ( tipo == "movie"){
+                        var title = elemento.title; // film
+                        var originalTitle = elemento.original_title; // film
+                    } else{
+                        var title = elemento.name; // serie TV
+                        var originalTitle = elemento.original_name; // serie TV
+                    }
+                    var flag = languageToFlag(elemento.original_language);
+                    var stars = voteToStars(elemento.vote_average);
+                    var posterUrl = getPoster(elemento.poster_path);
+
+                    var datiShow = {
+                        titolo: title,
+                        titoloOriginale: originalTitle,
+                        lingua: flag,
+                        voto: stars,
+                        poster: posterUrl
+                    }
+
+                    if (datiShow.titolo == datiShow.titoloOriginale){
+                        datiShow = {
+                            titolo: title,
+                            lingua: flag,
+                            voto: stars,
+                            poster: posterUrl
+                        }
+                    };
+                    if (datiShow.poster.endsWith("null")){
+                        datiShow.poster = "img/poster-default.svg"
+                    };
+
+
+
+
+                    var templateCompilato = templateResults(datiShow);
+                    $(".box-results").append(templateCompilato);
+
+
+                    $(".hai-cercato").show();
+                    $(".hai-cercato span").html(chiaveRicerca);
+                    $(".input-query-box").val("");
+                    $("main .box-ricerca").removeClass("active").addClass("hide");
+                    $("header .box-ricerca ").removeClass("hide").addClass("active");
+
+
+                }
+            },
+            error: function(err) {
+                alert("Errore");
+            }
+        });
+    };
+
+
+    // ===== ALTRE FUNZIONI =====
+
+    // clearResults: pulisce i risultati di ricerca prima di iniziarne una nuova
+
+    function clearResults(){
         $(".box-results").html("");
+    };
+
+    //  voteToStars: trasforma il voto da decimi a valore in stelle
+
+    function voteToStars(vote){
+        if (vote >= 0) {
+            vote = Math.ceil(vote / 2);
+        };
+        switch (vote) {
+            case 1:
+                vote = '<i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>';
+                break;
+            case 2:
+                vote = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>';
+                break;
+            case 3:
+                vote = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>';
+                break;
+            case 4:
+                vote = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i>';
+                break;
+            case 5:
+                vote = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>';
+                break;
+            default:
+                vote = "Nessuna valutazione";
+        };
+        return vote;
+    };
+
+    // languageToFlag: trasforma il codice lingua nella bandiera corrispondente
+
+    function languageToFlag(lang){
+
+        if (lang == "en"){
+            lang = "GB";
+        }
+        if (lang == "ja"){
+            lang = "JP";
+        }
+        if (lang == "da"){
+            lang = "DK";
+        }
+        if (lang == "el"){
+            lang = "GR";
+        }
+        if ((lang == "cs") ||
+            (lang == "nb") ||
+            (lang == "zh") ||
+            (lang == "hi") ||
+            (lang == "ta") ||
+            (lang == "te") ||
+            (lang == "ko") ||
+            (lang == "sq")){
+            var flag = "Altra";
+
+        } else {
+            var flag = '<img src="https://www.countryflags.io/' + lang + '/flat/64.png">';
+        };
+
+        return flag;
+    };
+
+    // getPoster: ottiene il poster del film o serie TV
+
+    function getPoster(path){
+        var posterBaseUrl = "https://image.tmdb.org/t/p/";
+        var resolution = "w342"
+        var posterUrl = posterBaseUrl + resolution + path;
+
+        return posterUrl;
 
     };
 
-    function ajaxCallFilm(){
-        var apiBaseUrl = "https://api.themoviedb.org/3/search/movie";
-        var chiaveRicerca = $(".input-query-box").val();
-        $.ajax({
-            url: apiBaseUrl + "",
-            data: {
-                api_key: "29ad81bd33ebd003f5307b9c32a17b2b",
-                language: "it-IT",
-                query: chiaveRicerca
-            },
-            method: "GET",
-            success: function(data) {
-                var movies = data.results;
-                for (var i = 0; i < movies.length; i++) {
-                    var movie = movies[i];
 
-                    var title = movie.title;
-                    var originalTitle = movie.original_title;
-                    var language = movie.original_language;
-                    if (movie.vote_average > 0) {
-                        var vote = Math.ceil(movie.vote_average / 2);
-                    };
-                    var posterUrl = movie.poster_path
-
-                    switch (vote) {
-                        case 1:
-                            vote = '<i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>';
-                            break;
-                        case 2:
-                            vote = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>';
-                            break;
-                        case 3:
-                            vote = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>';
-                            break;
-                        case 4:
-                            vote = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i>';
-                            break;
-                        case 5:
-                            vote = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>';
-                            break;
-                        default:
-                            vote = "ND";
-                            break;
-
-                    };
-
-                    var classeDaAggiungere = "";
-
-                    if (language == "en"){
-                        language = "GB";
-                    }
-                    if (language == "ja"){
-                        language = "JP";
-                    }
-                    if (language == "da"){
-                        language = "DK";
-                    }
-                    if (language == "el"){
-                        language = "GR";
-                    }
-
-                    if ((language == "cs") ||
-                        (language == "nb") ||
-                        (language == "zh") ||
-                        (language == "sq")){
-                        var flag = "Altra";
-                        classeDaAggiungere = "altra"
-
-
-                    } else {
-                        var flag = '<img src="https://www.countryflags.io/' + language + '/flat/64.png">';
-                    };
-
-                    var posterFullUrl = "https://image.tmdb.org/t/p/" + "w342" + posterUrl;
-
-                    var datiFilm = {
-                        titolo: title,
-                        titoloOriginale: originalTitle,
-                        lingua: flag,
-                        voto: vote,
-                        classLingua: classeDaAggiungere,
-                        poster: posterFullUrl
-                    };
-
-
-                    var templateCompilato = templateResults(datiFilm);
-                    $(".box-results").append(templateCompilato);
-                    $(".hai-cercato").show();
-                    $(".hai-cercato span").html(chiaveRicerca);
-                    $(".input-query-box").val("");
-                }
-            },
-            error: function(err) {
-                alert("Errore");
-            }
-        });
-    };
-
-    function ajaxCallSerie(){
-        var apiBaseUrl = "https://api.themoviedb.org/3/search/tv";
-        var chiaveRicerca = $(".input-query-box").val();
-        $.ajax({
-            url: apiBaseUrl + "",
-            data: {
-                api_key: "29ad81bd33ebd003f5307b9c32a17b2b",
-                language: "it-IT",
-                query: chiaveRicerca
-            },
-            method: "GET",
-            success: function(data) {
-                var movies = data.results;
-                for (var i = 0; i < movies.length; i++) {
-                    var movie = movies[i];
-
-                    var title = movie.name;
-                    var originalTitle = movie.original_name;
-                    var language = movie.original_language;
-                    if (movie.vote_average > 0) {
-                        var vote = Math.ceil(movie.vote_average / 2);
-                    };
-                    var posterUrl = movie.poster_path
-
-                    switch (vote) {
-                        case 1:
-                            vote = '<i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>';
-                            break;
-                        case 2:
-                            vote = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>';
-                            break;
-                        case 3:
-                            vote = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>';
-                            break;
-                        case 4:
-                            vote = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i>';
-                            break;
-                        case 5:
-                            vote = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>';
-                            break;
-                        default:
-                            vote = "ND";
-                            break;
-
-                    };
-
-                    var classeDaAggiungere = "";
-
-                    if (language == "en"){
-                        language = "GB";
-                    }
-                    if (language == "ja"){
-                        language = "JP";
-                    }
-                    if (language == "da"){
-                        language = "DK";
-                    }
-                    if (language == "el"){
-                        language = "GR";
-                    }
-
-                    if ((language == "cs") ||
-                        (language == "nb") ||
-                        (language == "zh") ||
-                        (language == "sq")){
-                        var flag = "Altra";
-                        classeDaAggiungere = "altra"
-
-
-                    } else {
-                        var flag = '<img src="https://www.countryflags.io/' + language + '/flat/64.png">';
-                    };
-
-
-                    var posterFullUrl = "https://image.tmdb.org/t/p/" + "w342" + posterUrl;
-
-                    var datiFilm = {
-                        titolo: title,
-                        titoloOriginale: originalTitle,
-                        lingua: flag,
-                        voto: vote,
-                        classLingua: classeDaAggiungere,
-                        poster: posterFullUrl
-                    };
-
-
-                    var templateCompilato = templateResults(datiFilm);
-                    $(".box-results").append(templateCompilato);
-                    $(".hai-cercato").show();
-                    $(".hai-cercato span").html(chiaveRicerca);
-                    $(".input-query-box").val("");
-                }
-            },
-            error: function(err) {
-                alert("Errore");
-            }
-        });
-    };
-
-    function apiCast(movieId) {
-        $.ajax({
-            url: "https://api.themoviedb.org/3/movie/" + movieId + "/credits",
-            data: {
-                api_key: "29ad81bd33ebd003f5307b9c32a17b2b",
-            },
-            method: "GET",
-            success: function(data) {
-                var castList = data.cast;
-                // getCast(cast);
-                console.log(castList);
-
-            },
-            error: function(err) {
-                alert("Errore ricerca cast");
-
-            }
-        });
-    };
-
-
-
-}); //=======================
+}); // fine document ready ========
